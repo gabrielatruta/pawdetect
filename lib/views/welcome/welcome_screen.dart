0f6064b2d2ff49c16e0b0fcef96303877e1a8790
gabrielatruta <gabriela.truta99@yahoo.com>
2025-08-22 18:29:22 +0300

    welcome screen working

--- lib/views/welcome/welcome_screen.dart
+++ lib/views/welcome/welcome_screen.dart
@@ -1,31 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:pawdetect/views/welcome/widgets/welcome_guest_option.dart';
-import 'package:pawdetect/views/shared/primary_button.dart';
-import 'package:pawdetect/views/shared/secondary_button.dart';
 import 'package:provider/provider.dart';
-import '../../../../viewmodels/welcome_viewmodel.dart';
+import '../../../viewmodels/welcome_viewmodel.dart';
+import '../../../styles/app_colors.dart';
+import 'widgets/welcome_header.dart';
+import 'widgets/welcome_logo.dart';
+import 'widgets/welcome_title.dart';
+import 'widgets/welcome_description.dart';
+import 'widgets/welcome_actions.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final vm = context.read<WelcomeViewModel>();
-
-    return Column(
-      children: [
-        PrimaryButton(
-          text: "Log In",
-          onPressed: () => vm.goToLogin(context),
+    return ChangeNotifierProvider(
+      create: (_) => WelcomeViewModel(),
+      child: Scaffold(
+        backgroundColor: AppColors.white,
+        appBar: const WelcomeHeader(),
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                WelcomeLogo(),
+                SizedBox(height: 32),
+                WelcomeTitle(),
+                SizedBox(height: 12),
+                WelcomeDescription(),
+                SizedBox(height: 40),
+                WelcomeActions(),
+              ],
+            ),
+          ),
         ),
-        const SizedBox(height: 16),
-        SecondaryButton(
-          text: "Sign Up",
-          onPressed: () => vm.goToSignup(context),
-        ),
-        const SizedBox(height: 16),
-        const WelcomeGuestOption(),
-      ],
+      ),
     );
   }
 }
