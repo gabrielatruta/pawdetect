cdab0796ec2e8cb0a8dc5ca3ba151fcb1151ee61
gabrielatruta <gabriela.truta99@yahoo.com>
2025-08-25 16:50:17 +0300

    add new report bottom navigation

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -10,7 +10,7 @@ import '../../../styles/app_colors.dart';
 class HomeScreen extends StatelessWidget {
   final bool useLocation;
   const HomeScreen({super.key, bool? useLocation})
-      : useLocation = useLocation ?? true; 
+    : useLocation = useLocation ?? true;
 
   @override
   Widget build(BuildContext context) {
@@ -25,18 +25,24 @@ class HomeScreen extends StatelessWidget {
           children: [
             const SizedBox(height: 16),
             HomeMapCard(useLocation: useLocation),
-
-            const SizedBox(height: 200),
-            PrimaryButton(
-              text: "Add new report",
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => const AddNewReportScreen()),
-                );
-              },
-            ),
+            const SizedBox(height: 16),
           ],
+        ),
+      ),
+
+      // add new report button
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 8, 16, 16),
+          child: PrimaryButton(
+            text: "Add new report",
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const AddNewReportScreen()),
+              );
+            },
+          ),
         ),
       ),
     );
