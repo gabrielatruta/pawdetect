522c05825fbbbe1b9609dc5eed727d6edea80ebc
gabrielatruta <gabriela.truta99@yahoo.com>
2025-08-31 20:09:09 +0300

    no previous user data stored for guests

--- lib/viewmodels/welcome_viewmodel.dart
+++ lib/viewmodels/welcome_viewmodel.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:pawdetect/views/auth/login_screen.dart';
 import 'package:pawdetect/views/auth/signup_screen.dart';
@@ -18,10 +19,15 @@ class WelcomeViewModel extends ChangeNotifier {
     );
   }
 
-  void continueAsGuest(BuildContext context) {
-    Navigator.push(
+  Future<void> continueAsGuest(BuildContext context) async {
+    // ensure any previous session is cleared
+    await FirebaseAuth.instance.signOut();
+
+    // go to Home and remove auth screens from back stack
+    Navigator.pushAndRemoveUntil(
       context,
       MaterialPageRoute(builder: (_) => const HomeScreen()),
+      (route) => false,
     );
   }
 }
